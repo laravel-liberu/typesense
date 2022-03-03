cb0fbf4d83dc85ff8e0a44b7cb2e4ebf810d5573
Mihai <mihai.ocneanu@gmail.com>
2022-03-03 13:31:32 +0200

    updated TemplatePath visibility

--- src/Forms/Builders/Settings.php
+++ src/Forms/Builders/Settings.php
@@ -7,10 +7,15 @@ use LaravelEnso\Typesense\Models\Settings as Model;
 
 class Settings
 {
-    protected const TemplatePath = __DIR__.'/../Templates/settings.json';
+    private const TemplatePath = __DIR__.'/../Templates/settings.json';
 
     public function edit(Model $settings)
     {
-        return (new Form(static::TemplatePath))->edit($settings);
+        return (new Form($this->templatePath()))->edit($settings);
+    }
+
+    public function templatePath(): string
+    {
+        return self::TemplatePath;
     }
 }
